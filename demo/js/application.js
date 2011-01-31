891b24e4914c150bfc4bb81aa4f1dba1672d07ad
citrus <spencer@citrusme.com>
2011-01-30 17:41:17 -0800

    updated demo

--- demo/js/application.js
+++ demo/js/application.js
@@ -1,20 +1,10 @@
 (function($){
   $(document).ready(function(){
 
-    $('#content a').tipsy({ gravity: 'e' }).click(function(evt) {
+    $('#content a').tipsy({ gravity: 'w' }).click(function(evt) {
       evt.preventDefault();
       $(this).tipsy('load', this.href);
     });
-    
-    //.click(function(evt) {
-    //  evt.preventDefault();
-    //  
-    //  //var tip = $(this).tipsy(true);      
-    //  //console.log('loading', tip);
-    //
-    //  //tip.load(this.href);
-    //  
-    //});
     
   });
 })(jQuery);
